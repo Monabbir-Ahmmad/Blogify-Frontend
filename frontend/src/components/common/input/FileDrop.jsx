7bc834fabc010bde4cbaebc51754c9d4eec84f1d
Monabbir Ahmmad Tajbir <monabbir.ahmmad@gmail.com>
2023-05-29 22:46:14 +0600

    Drag and drop added

--- frontend/src/components/common/input/FileDrop.jsx
+++ frontend/src/components/common/input/FileDrop.jsx
@@ -1,21 +1,105 @@
+import { FiX as RemoveIcon } from "react-icons/fi";
 import imageUpload from "../../../assets/imageUpload.svg";
+import { twMerge } from "tailwind-merge";
+import { useState } from "react";
 
-function FileDrop() {
+function FileDrop({
+  maxSizeKB = 1024 * 5,
+  allowedMimeTypes = ["image/png", "image/jpg", "image/jpeg"],
+}) {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [dragging, setDragging] = useState(false);
+
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    setDragging(true);
+  };
+
+  const handleDragLeave = (e) => {
+    e.preventDefault();
+    setDragging(false);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setDragging(false);
+    const file = e.dataTransfer.files[0];
+    if (file) {
+      validateFile(file);
+    }
+  };
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      validateFile(file);
+    }
+  };
+
+  const validateFile = (file) => {
+    if (file.size <= maxSizeKB * 1024) {
+      if (allowedMimeTypes.includes(file.type)) {
+        setSelectedFile(file);
+      } else {
+        alert("Invalid file type. Please choose a valid file type.");
+      }
+    } else {
+      alert(`File size exceeds the maximum limit of ${maxSizeKB}KB.`);
+    }
+  };
+
+  const handleRemoveImage = () => {
+    setSelectedFile(null);
+  };
+
   return (
-    <div className="flex items-center justify-center w-full">
-      <label
-        htmlFor="dropzone-file"
-        className="overflow-hidden flex flex-col items-center justify-center w-full border-2 border-slate-300 border-dashed rounded cursor-pointer bg-slate-50"
-      >
-        <div className="flex flex-col items-center justify-center p-5">
-          <img src={imageUpload} alt="Image upload" className="-mt-6" />
-          <p className="mb-2 text-sm">
-            <span className="font-semibold">Click to upload image</span>
-          </p>
-          <p className="text-xs">PNG, JPG or JPEG (MAX. 5MB)</p>
+    <div
+      className={twMerge("drag-drop-container", dragging && "dragging")}
+      onDragEnter={handleDragEnter}
+      onDragLeave={handleDragLeave}
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+    >
+      {selectedFile ? (
+        <div className="relative">
+          <img
+            src={URL.createObjectURL(selectedFile)}
+            alt="Selected"
+            className="object-cover h-64 w-full rounded-xl"
+          />
+          <button
+            onClick={handleRemoveImage}
+            className="icon-btn-error absolute top-4 right-4"
+          >
+            <RemoveIcon size={20} />
+          </button>
         </div>
-        <input id="dropzone-file" type="file" className="hidden" />
-      </label>
+      ) : (
+        <div>
+          <label
+            htmlFor="dropzone-file"
+            className="cursor-pointer overflow-hidden w-full p-4 flex flex-col items-center justify-center border-2 border-slate-300 border-dashed rounded bg-slate-50 hover:bg-slate-100 transition-colors duration-200 ease-in-out"
+          >
+            <img src={imageUpload} alt="Upload" className="-mt-6 h-44" />
+            <p className="mb-2 text-sm">
+              <span className="font-semibold">Click to upload</span> or drag and
+              drop
+            </p>
+            <p className="text-xs">PNG, JPG or JPEG (MAX. 5MB)</p>
+            <input
+              id="dropzone-file"
+              type="file"
+              accept="image/*"
+              onChange={handleFileInputChange}
+              className="hidden"
+            />
+          </label>
+        </div>
+      )}
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,5 @@
 const themeColors = {
   background: "var(--color-background)",
-  backgroundLight: "var(--color-background-light)",
 
   primary: "var(--color-primary)",
   primaryDark: "var(--color-primary-dark)",
@@ -10,10 +9,12 @@ const themeColors = {
   accent: "var(--color-accent)",
   accentDark: "var(--color-accent-dark)",
   accentLight: "var(--color-accent-light)",
+  accentLighter: "var(--color-accent-lighter)",
 
   error: "var(--color-error)",
   errorDark: "var(--color-error-dark)",
   errorLight: "var(--color-error-light)",
+  errorLighter: "var(--color-error-lighter)",
 };
 
 /** @type {import('tailwindcss').Config} */
