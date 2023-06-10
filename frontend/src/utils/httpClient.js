baaf31901343466f16ad3572a18cb31a1fc439b9
Monabbir Ahmmad Tajbir <monabbir.ahmmad@gmail.com>
2023-06-10 18:05:01 +0600

    http client bug fix

--- frontend/src/utils/httpClient.js
+++ frontend/src/utils/httpClient.js
@@ -7,12 +7,12 @@ const refreshTokenInterceptor = async (err) => {
   const originalConfig = err.config;
 
   if (err.response?.status === 401 && !originalConfig._retry) {
-    originalConfig._retry = true;
-
     try {
       await authService.refreshAccessToken();
 
-      return httpClient(originalConfig);
+      originalConfig._retry = true;
+
+      return await httpClient(originalConfig);
     } catch (error) {
       storageService.removeAuthData();
 
