0574b0317ea99a3eeb67f066d4535f94cc01589b
Monabbir Ahmmad Tajbir <monabbir.ahmmad@gmail.com>
2023-07-13 17:00:31 +0600

    update: Project dockerized

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,13 @@
-import { defineConfig, loadEnv } from "vite";
-
-import http from "https";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
     server: {
+      host: "0.0.0.0",
       port: 3000,
-      proxy: {
-        [env.VITE_API]: {
-          target: env.VITE_API_HOST,
-          changeOrigin: true,
-          secure: true,
-          agent: new http.Agent(),
-        },
-      },
+      cors: true,
     },
     test: {
       globals: true,
